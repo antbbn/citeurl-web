fd1b56f08247c29611dbeb8499b16a1472c95597
Antonio Bibiano <antbbn@gmail.com>
2014-08-31 05:10:43 +0000

    Made match case insensitive

--- sources/__init__.py
+++ sources/__init__.py
@@ -13,7 +13,7 @@ list.append(jcap.matcher)
 
 def match(obj, cit):
   for pattern in obj.patterns:
-    result = re.search(pattern,cit)
+    result = re.search(pattern,cit,re.IGNORECASE)
     if result:
       [setattr(obj,field,result.group(field)) for field in obj.fields]
       return True
